79d48d8a15af3f2c0793ee7f9fed9e1121386d23
Jesse Freeman <jessefreeman@gmail.com>
2025-11-08 17:29:00 +0000

    fix: Use module.agent.agent_id instead of coder_agent.main.id

--- config/coder/templates/node-template/module-node-modules.tf
+++ config/coder/templates/node-template/module-node-modules.tf
@@ -1,6 +1,6 @@
 module "node_modules_persistence" {
   source             = "git::https://github.com/weekend-code-project/weekendstack.git//config/coder/templates/git-modules/node-modules-persistence?ref=v0.1.1"
-  agent_id           = coder_agent.main.id
+  agent_id           = module.agent.agent_id
   node_modules_paths = data.coder_parameter.node_modules_paths.value
   workspace_folder   = "/home/coder/workspace"
   persist_folder     = "/home/coder/.persist"
