34e192ab64e5d54ac0c3ab95cfbaf104ada242ad
Jesse Freeman <jessefreeman@gmail.com>
2025-11-09 14:27:15 +0000

    Restore all parameters - previous approach won't work

--- config/coder/templates/shared-template-modules/module-docker.tf
+++ config/coder/templates/shared-template-modules/module-docker.tf
@@ -1,12 +1,11 @@
-# PARAMETER REMOVED FOR TESTING - see module-docker.tf.with-params
-# data "coder_parameter" "enable_docker" {
-#   name         = "enable_docker"
-#   display_name = "Enable Docker-in-Docker"
-#   description  = "Install and run Docker daemon inside the workspace for container development."
-#   type         = "bool"
-#   form_type    = "switch"
-#   default      = false
-#   mutable      = false
-#   order        = 30
-# }
+data "coder_parameter" "enable_docker" {
+  name         = "enable_docker"
+  display_name = "Enable Docker-in-Docker"
+  description  = "Install and run Docker daemon inside the workspace for container development."
+  type         = "bool"
+  form_type    = "switch"
+  default      = false
+  mutable      = false
+  order        = 30
+}
 module "docker" { source = "git::https://github.com/weekend-code-project/weekendstack.git//config/coder/templates/git-modules/docker-integration?ref=v0.1.0" }
--- config/coder/templates/shared-template-modules/module-metadata.tf
+++ config/coder/templates/shared-template-modules/module-metadata.tf
@@ -1,50 +1,50 @@
-# data "coder_parameter" "metadata_blocks" {
-#   name         = "metadata_blocks"
-#   display_name = "Metadata Blocks"
-#   description  = "Select metadata blocks to display."
-#   type         = "list(string)"
-#   form_type    = "multi-select"
-#   default      = jsonencode(["cpu", "ram", "disk", "ports"])
-#   mutable      = true
-#   order        = 50
-# 
-#   option {
-#     name  = "CPU Usage"
-#     value = "cpu"
-#   }
-# 
-#   option {
-#     name  = "RAM Usage"
-#     value = "ram"
-#   }
-# 
-#   option {
-#     name  = "Disk Usage"
-#     value = "disk"
-#   }
-# 
-#   option {
-#     name  = "Ports"
-#     value = "ports"
-#     icon  = "/icon/network.svg"
-#   }
-# 
-#   option {
-#     name  = "SSH Port"
-#     value = "ssh_port"
-#     icon  = "/icon/terminal.svg"
-#   }
-# 
-#   option {
-#     name  = "Home Directory"
-#     value = "home_dir"
-#   }
-# 
-#   option {
-#     name  = "Container Image"
-#     value = "image"
-#   }
-# }
+data "coder_parameter" "metadata_blocks" {
+  name         = "metadata_blocks"
+  display_name = "Metadata Blocks"
+  description  = "Select metadata blocks to display."
+  type         = "list(string)"
+  form_type    = "multi-select"
+  default      = jsonencode(["cpu", "ram", "disk", "ports"])
+  mutable      = true
+  order        = 50
+
+  option {
+    name  = "CPU Usage"
+    value = "cpu"
+  }
+
+  option {
+    name  = "RAM Usage"
+    value = "ram"
+  }
+
+  option {
+    name  = "Disk Usage"
+    value = "disk"
+  }
+
+  option {
+    name  = "Ports"
+    value = "ports"
+    icon  = "/icon/network.svg"
+  }
+
+  option {
+    name  = "SSH Port"
+    value = "ssh_port"
+    icon  = "/icon/terminal.svg"
+  }
+
+  option {
+    name  = "Home Directory"
+    value = "home_dir"
+  }
+
+  option {
+    name  = "Container Image"
+    value = "image"
+  }
+}
 
 module "metadata" {
   source         = "git::https://github.com/weekend-code-project/weekendstack.git//config/coder/templates/git-modules/metadata?ref=v0.1.0"
