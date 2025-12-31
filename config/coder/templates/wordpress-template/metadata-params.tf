e8170a19dc39b28bbf01614bc7ed75c13a5a6832
Jesse Freeman <jessefreeman@gmail.com>
2025-12-31 20:07:18 +0000

    Fix metadata module parameters in WordPress template
    - Changed selected_blocks to enabled_blocks (correct parameter name)
    - Removed agent_id parameter (not used by metadata module)

--- config/coder/templates/wordpress-template/metadata-params.tf
+++ config/coder/templates/wordpress-template/metadata-params.tf
@@ -7,7 +7,6 @@
 module "metadata" {
   source = "git::https://github.com/weekend-code-project/weekendstack.git//config/coder/template-modules/modules/metadata-module?ref=PLACEHOLDER"
   
-  agent_id        = module.agent.agent_id
-  selected_blocks = ["cpu", "ram", "disk"]  # Basic monitoring for WordPress
-  custom_blocks   = local.all_custom_metadata
+  enabled_blocks = ["cpu", "ram", "disk"]  # Basic monitoring for WordPress
+  custom_blocks  = local.all_custom_metadata
 }
